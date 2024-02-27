92f2bed97d6b06904f2c05d11efe8146ac466924
Martin Renteria <mrrenteria0159@gmail.com>
2024-02-26 17:27:43 -0700

    home page restructured

--- React-Dev-Portfolio/src/App.jsx
+++ React-Dev-Portfolio/src/App.jsx
@@ -1,29 +1,19 @@
 import React, {useState} from 'react';
 import Intro from './components/Intro';
 import Timeline from './components/Timeline';
-import Portfolio from './components/Portfolio';
+import Portfolio from './components/Portfolio'
 import Footer from './components/Footer';
 import Contact from './components/Contact';
 import { render } from 'react-dom';
-import Header from './components/Header';
+
+import './style/style.css';
 function App() {
-    const [currentPage, setCurrentPage] = useState('Home')
-    const renderPage = () => {
-        if (currentPage === 'Home') {
-          return <Intro />;
-        }
-        if (currentPage === 'Portfolio') {
-          return <Portfolio />;
-        }
-        return <Contact />;
-      };
-    
-      const handlePageChange = (page) => setCurrentPage(page);
     return (
      <div className='App'>
-        <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+        {/* <Header currentPage={currentPage} handlePageChange={handlePageChange} /> */}
         <main>
-            {renderPage()}
+         <Intro /> 
+            
         </main>
 
      </div>
